1b7f314adee780a3f6902373587a62f0ea96abd1
HadisehPourali <h.pourali.a@gmail.com>
2024-10-27 11:28:24 +0330

    fixing

--- app.py
+++ app.py
@@ -237,5 +237,5 @@ def login():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    port = int(os.getenv('PORT', 5000))
+    port = 5000
     app.run(host='0.0.0.0', port=port)
